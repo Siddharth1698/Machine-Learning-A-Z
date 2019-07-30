c3aa082102726c8953f687c25b4116fd11958f7e
root <root@localhost.localdomain>
2019-07-31 00:41:37 +0530

    feature scaling

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -33,3 +33,23 @@ y = labelencoder_y.fit_transform(y)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+from sklearn.preprocessing import StandardScaler
+
+
+sc_X = StandardScaler()
+
+
+X_train = sc_X.fit_transform(X_train)
+
+
+X_test = sc_X.transform(X_test)
+
+
+sc_y = StandardScaler()
+
+
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+
+
