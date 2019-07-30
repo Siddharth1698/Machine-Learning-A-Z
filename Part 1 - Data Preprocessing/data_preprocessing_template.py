e0e5ddf071fa7f1d68190cdead56da536449309d
root <root@localhost.localdomain>
2019-07-31 00:29:15 +0530

    splitting the dataset into training and testing set

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -29,3 +29,7 @@ X = np.array(ct.fit_transform(X), dtype=np.float)
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
