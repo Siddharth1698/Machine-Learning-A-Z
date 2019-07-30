494ec9d512718a6c5f742605ebf79c17174a6bf2
root <root@localhost.localdomain>
2019-07-31 00:14:40 +0530

    Categorical data

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -14,3 +14,18 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+from sklearn.compose import ColumnTransformer
+     
+ct = ColumnTransformer(
+[('one_hot_encoder', OneHotEncoder(), [0])],   # The column numbers to be transformed (here is [0] but can be [0, 1, 3])
+remainder='passthrough'                         # Leave the rest of the columns untouched
+)
+     
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
