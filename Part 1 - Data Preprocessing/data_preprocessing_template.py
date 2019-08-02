32eb041464e694befa213afc052485019ac2ecda
Siddharth M <siddharth.m98@gmail.com>
2019-08-02 15:19:43 +0530

    Changes

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -26,13 +26,30 @@ remainder='passthrough'                         # Leave the rest of the columns 
 )
      
 X = np.array(ct.fit_transform(X), dtype=np.float)
+
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
 from sklearn.preprocessing import StandardScaler
+
+
 sc_X = StandardScaler()
+
+
 X_train = sc_X.fit_transform(X_train)
+
+
 X_test = sc_X.transform(X_test)
+
+
 sc_y = StandardScaler()
+
+
 y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+
+
--- Simple_Linear_Regression/simple_linear_regression.py
+++ Simple_Linear_Regression/simple_linear_regression.py
@@ -11,11 +11,11 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -24,3 +24,23 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train,y_train,color ='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('salary vs experienc (training set)')
+plt.xlabel('years of exp')
+plt.ylabel('salary')
+plt.show()
+
+plt.scatter(X_test,y_test,color ='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('salary vs experienc (testing set)')
+plt.xlabel('years of exp')
+plt.ylabel('salary')
+plt.show()
