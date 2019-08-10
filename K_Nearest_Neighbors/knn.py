a27960e6c5ba964c9ed83e597a02151fbfdc2fa1
Siddharth M <siddharth.m98@gmail.com>
2019-08-10 14:48:00 +0530

    KNN

---
+++ K_Nearest_Neighbors/knn.py
@@ -0,0 +1,31 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+classifier.fit(X_train,y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+# Logistic Regression
+
